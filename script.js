e605597cb293be7a1e7af446af4535e8ec4dd06c
Alda-design <design@alda.ai>
2025-04-18 14:05:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 document.getElementById("infoForm").addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const form = e.target;
-  const userData = Object.fromEntries(new FormData(form));
+  const formElement = e.target;
+  const userData = Object.fromEntries(new FormData(formElement));
 
   const existingPdfBytes = await fetch("kbfintech_template.pdf").then(res => res.arrayBuffer());
   const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
-  const formFields = pdfDoc.getForm();
 
-  formFields.getTextField("KOR_NAME").setText(userData.kor_name);
-  formFields.getTextField("ENG_NAME").setText(userData.eng_name);
-  formFields.getTextField("KOR_DEPT").setText(userData.kor_dept);
-  formFields.getTextField("ENG_DEPT").setText(userData.eng_dept);
-  formFields.getTextField("KOR_TITLE").setText(userData.kor_title);
-  formFields.getTextField("ENG_TITLE").setText(userData.eng_title);
-  formFields.getTextField("PHONE").setText(userData.phone);
-  formFields.getTextField("EMAIL_ID").setText(userData.email_id);
+  const form = pdfDoc.getForm();
+
+  // 👇 이 부분 추가: PDF에 텍스트 필드 모양 생성
+  form.updateFieldAppearances(PDFLib.StandardFonts.Helvetica);
+
+  form.getTextField("KOR_NAME").setText(userData.kor_name);
+  form.getTextField("ENG_NAME").setText(userData.eng_name);
+  form.getTextField("KOR_DEPT").setText(userData.kor_dept);
+  form.getTextField("ENG_DEPT").setText(userData.eng_dept);
+  form.getTextField("KOR_TITLE").setText(userData.kor_title);
+  form.getTextField("ENG_TITLE").setText(userData.eng_title);
+  form.getTextField("PHONE").setText(userData.phone);
+  form.getTextField("EMAIL_ID").setText(userData.email_id);
 
   const pdfBytes = await pdfDoc.save();
   const blob = new Blob([pdfBytes], { type: "application/pdf" });
